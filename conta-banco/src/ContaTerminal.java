e44c2bbf52ef1081bd50ad495e22e6a36b629e97
Emerson Okada <lkoemerson@live.com>
2024-07-01 14:25:01 -0300

    scanner conta

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner conta = new conta(System.in);
+
     }
 }
